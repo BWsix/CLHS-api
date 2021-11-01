f5c8646cda43cc0d54765ca7597fcc1be98fcf9a
BWsix <itisbw62@gmail.com>
2021-11-01 20:55:41 +0800

    feat(api): added a method block

--- api/pages/api/news/[id].ts
+++ api/pages/api/news/[id].ts
@@ -5,6 +5,12 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  if (req.method !== "GET")
+    return res.status(405).json({
+      error: true,
+      detail: `${req.method!.toLowerCase()} request is not allowed.`,
+    });
+
   const id = req.query.id as string;
 
   const result = await newsContentQuery(id);
--- api/pages/api/news/index.ts
+++ api/pages/api/news/index.ts
@@ -1,8 +1,22 @@
 import { newsListQuery } from "@clhs-api/core";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(_: NextApiRequest, res: NextApiResponse) {
-  const result = await newsListQuery();
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  switch (req.method) {
+    case "GET":
+      return res.json(await newsListQuery());
 
-  res.json(result);
+    case "POST":
+      const params = req.body;
+      return res.json(await newsListQuery(params));
+
+    default:
+      return res.status(405).json({
+        error: true,
+        detail: `${req.method!.toLowerCase()} request is not allowed.`,
+      });
+  }
 }
